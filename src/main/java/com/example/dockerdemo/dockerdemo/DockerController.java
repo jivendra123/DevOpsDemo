0a80dcbebeb524d21fef00b9a5c14328816fc356
Jivendra Kumar <49364310+jivendra123@users.noreply.github.com>
2021-10-21 12:24:32 +0530

    Update DockerController.java

--- src/main/java/com/example/dockerdemo/dockerdemo/DockerController.java
+++ src/main/java/com/example/dockerdemo/dockerdemo/DockerController.java
@@ -1,7 +1,7 @@
 package com.example.dockerdemo.dockerdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerController {
